403cf8fc50f1a391320e51468d44bad8e57b982f
Bruno Tavares <connect+github@bltavares.com>
2017-06-13 16:55:03 -0300

    Adiciona script para notificar bots de crawling

---
+++ ping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+declare -a sites=(
+    'https://www.google.com/webmasters/sitemaps/ping?sitemap=https://blog.bltavares.com/atom.xml'
+    'https://www.bing.com/webmaster/ping.aspx?siteMap=https://blog.bltavares.com/atom.xml'
+    'https://blogs.yandex.ru/pings/?status=success&url=https://blog.bltavares.com/atom.xml'
+)
+
+for x in "${sites[@]}"; do
+    set -x
+    curl --silent "$x" > /dev/null
+    set +x
+done
